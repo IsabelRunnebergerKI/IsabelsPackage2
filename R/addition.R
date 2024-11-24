3c6322b5664cb0db34ccad24bd3003d8198cb0b4
IsabelRunnebergerKI <isabel.runneberger@ki.se>
2024-11-24 22:53:43 +0100

    Initial commit
    Initial files

---
+++ R/addition.R
@@ -0,0 +1,11 @@
+#' @title Addition of x and y
+#' @param x is a number
+#' @param y is another number
+#' @return The sum
+#'
+#' @examples x + y = 5
+#'
+
+addition <- function (x = 4, y = 1) {
+  x + y
+}
